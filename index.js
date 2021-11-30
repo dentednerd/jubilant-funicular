fabe6b8c233581e44300802ca23ab0208978cea1
Joey Imlay <joey@Joeys-MacBook-Pro-2.local>
2021-11-30 11:06:54 +0000

    add ESLint and Husky

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const sayFaveColour = (person) => {
   const faveColours = {
-    "August": "purple",
-    "Haz": "atomic tangerine",
-    "Poonam": "ube",
-    "Paul": "viridian green",
-    "Ant": "deeppink",
-    "Sarah": "cobalt"
+    August: 'purple',
+    Haz: 'atomic tangerine',
+    Poonam: 'ube',
+    Paul: 'viridian green',
+    Ant: 'deeppink',
+    Sarah: 'cobalt',
   };
 
-  console.log(`${person}'s favourite colour is ${faveColours[person]}.`)
+  console.log(`${person}'s favourite colour is ${faveColours[person]}.`);
 };
 
-sayFaveColour("August");
-sayFaveColour("Haz");
+sayFaveColour('August');
+sayFaveColour('Haz');
